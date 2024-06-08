ced9977dd0b8e7a6f224712f19dd6dc6c883a868
strixer1605 <strixer1605@gmail.com>
2024-06-08 17:14:38 -0300

    nash2

--- main/prediccion.php
+++ main/prediccion.php
@@ -19,11 +19,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/prediccion.css">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Gabarito:wght@400..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/prediccion.css">
     <style>
         /* Estilos para eliminar las flechas de los inputs tipo number */
         input[type=number] {
